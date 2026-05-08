648a9b2f01b8ad80face43601fca1eaa379e4425
Kavya Cherukuri <kavyacherukuri21@gmail.com>
2026-05-08 16:55:07 +0530

    old controller

--- demogit1/src/main/java/com/kavyatrainings/demo/controller/addressController.java
+++ demogit1/src/main/java/com/kavyatrainings/demo/controller/addressController.java
@@ -3,29 +3,28 @@ package com.kavyatrainings.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kavyatrainings.demo.entities.address;
 import com.kavyatrainings.demo.service.addressService;
 
 @RestController
 public class addressController {
+	
+	@Autowired
+	addressService addSrv;
+	
+	@RequestMapping("/addresses")
+	public List<address> getAddress(){
+		return addSrv.getAddress();
+	}
+	
+	@PostMapping("/addresses")
+	public address createAddress(@RequestBody address payload) {
+		return addSrv.createAddress(payload);
+	}
 
-    @Autowired
-    addressService addSrv;
-
-    @RequestMapping("/")
-    public String home() {
-        return "Spring Boot Application Running Successfully";
-    }
-
-    @RequestMapping("/addresses")
-    public List<address> getAddress() {
-        return addSrv.getAddress();
-    }
-
-    @PostMapping("/addresses")
-    public address createAddress(@RequestBody address payload) {
-        return addSrv.createAddress(payload);
-    }
 }
